38194b51b4a5bbbe3868293a2d241f0fcc25cbb6
Zepheri <dz.ignacioaguilar@gmail.com>
2026-07-04 10:28:00 -0400

    v1.2.4

--- unica/configs/version.sh
+++ unica/configs/version.sh
@@ -17,7 +17,7 @@
 
 VERSION_MAJOR=1
 VERSION_MINOR=2
-VERSION_PATCH=3
+VERSION_PATCH=4
 ROM_VERSION="${VERSION_MAJOR}.${VERSION_MINOR}.${VERSION_PATCH}"
 # Append current commit hash to version name
 ROM_VERSION+="-$(git rev-parse --short HEAD)"
